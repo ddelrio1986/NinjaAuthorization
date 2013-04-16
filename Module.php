279f6ab044e3cedfa392eab3bdf7933443975286
Daniel Del Rio <ddelrio1986@gmail.com>
2013-04-16 14:56:48 -0400

    Add some todos for future reference

--- Module.php
+++ Module.php
@@ -1,3 +1,9 @@
 <?php
+/**
+ * @todo Define module dependencies.
+ * @todo Setup the autoload_* files that ZF modules should have in their root.
+ * @todo Complete the unit tests.
+ * @todo Ensure that all services registered in the service manager have a vendor prefix.
+ */
 
 require_once __DIR__ . '/src/NinjaAuthorization/Module.php';
