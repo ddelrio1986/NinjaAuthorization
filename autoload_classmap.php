ec98bd8e9bb239bb080f2446a7b502305b53ebc3
Daniel Del Rio <ddelrio1986@gmail.com>
2013-04-16 14:57:24 -0400

    Add an empty classmap file

---
+++ autoload_classmap.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * Classmap
+ * 
+ * This is the classmap for the module which can be used to setup autoloading.
+ * 
+ * @author Daniel Del Rio <daniel@aelearn.com>
+ * @package NinjaAuthorization
+ * @filesource
+ */
+
+return array();
