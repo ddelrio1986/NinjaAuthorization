eefa310455bed9590e8e004e2dfcbdc23aa05f79
Daniel Del Rio <ddelrio1986@gmail.com>
2013-05-11 13:49:39 -0400

    Add progress so far

--- data/NinjaAuthorization.sql
+++ data/NinjaAuthorization.sql
@@ -1,4 +1,4 @@
-CREATE  TABLE IF NOT EXISTS `NinjaAuthorization`.`user` (
+CREATE  TABLE IF NOT EXISTS `user` (
   `id` INT NOT NULL AUTO_INCREMENT ,
   `username` VARCHAR(20) NOT NULL ,
   `password` VARCHAR(20) NOT NULL ,
@@ -6,7 +6,7 @@ CREATE  TABLE IF NOT EXISTS `NinjaAuthorization`.`user` (
   UNIQUE INDEX `username_UNIQUE` (`username` ASC) )
 ENGINE = InnoDB;
 
-CREATE  TABLE IF NOT EXISTS `NinjaAuthorization`.`role` (
+CREATE  TABLE IF NOT EXISTS `role` (
   `id` INT NOT NULL AUTO_INCREMENT ,
   `name` VARCHAR(25) NOT NULL ,
   `parent_role_id` INT NULL DEFAULT NULL ,
@@ -14,27 +14,27 @@ CREATE  TABLE IF NOT EXISTS `NinjaAuthorization`.`role` (
   UNIQUE INDEX `name_UNIQUE` (`name` ASC) )
 ENGINE = InnoDB;
 
-CREATE  TABLE IF NOT EXISTS `NinjaAuthorization`.`role_assignment` (
+CREATE  TABLE IF NOT EXISTS `role_assignment` (
   `id` INT NOT NULL AUTO_INCREMENT ,
   `user_id` INT NOT NULL ,
   `role_id` INT NOT NULL ,
   PRIMARY KEY (`id`) )
 ENGINE = InnoDB;
 
-CREATE  TABLE IF NOT EXISTS `NinjaAuthorization`.`resource` (
+CREATE  TABLE IF NOT EXISTS `resource` (
   `id` INT NOT NULL AUTO_INCREMENT ,
   `name` VARCHAR(45) NOT NULL ,
   PRIMARY KEY (`id`) ,
   UNIQUE INDEX `name_UNIQUE` (`name` ASC) )
 ENGINE = InnoDB;
 
-CREATE  TABLE IF NOT EXISTS `NinjaAuthorization`.`privilege` (
+CREATE  TABLE IF NOT EXISTS `privilege` (
   `id` INT NOT NULL AUTO_INCREMENT ,
   `name` VARCHAR(45) NOT NULL ,
   PRIMARY KEY (`id`) )
 ENGINE = InnoDB;
 
-CREATE  TABLE IF NOT EXISTS `NinjaAuthorization`.`permission` (
+CREATE  TABLE IF NOT EXISTS `permission` (
   `id` INT NOT NULL AUTO_INCREMENT ,
   `role_id` INT NULL ,
   `user_id` INT NULL ,
