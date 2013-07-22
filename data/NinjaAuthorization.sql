7fb7854c1293a114dd55c4ebdeb4038f5d8d2d0d
Daniel Del Rio <ddelrio1986@gmail.com>
2013-07-22 10:13:41 -0400

    Update relationships

--- data/NinjaAuthorization.sql
+++ data/NinjaAuthorization.sql
@@ -2,13 +2,11 @@ SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0;
 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0;
 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='TRADITIONAL';
 
-CREATE SCHEMA IF NOT EXISTS `NinjaAuthorization` DEFAULT CHARACTER SET utf8 COLLATE utf8_general_ci ;
-USE `NinjaAuthorization` ;
 
 -- -----------------------------------------------------
--- Table `NinjaAuthorization`.`user`
+-- Table `user`
 -- -----------------------------------------------------
-CREATE  TABLE IF NOT EXISTS `NinjaAuthorization`.`user` (
+CREATE  TABLE IF NOT EXISTS `user` (
   `id` INT NOT NULL AUTO_INCREMENT ,
   `deleted` TINYINT(1) NOT NULL DEFAULT 0 ,
   `date_added` DATETIME NOT NULL ,
@@ -18,9 +16,9 @@ ENGINE = InnoDB;
 
 
 -- -----------------------------------------------------
--- Table `NinjaAuthorization`.`role`
+-- Table `role`
 -- -----------------------------------------------------
-CREATE  TABLE IF NOT EXISTS `NinjaAuthorization`.`role` (
+CREATE  TABLE IF NOT EXISTS `role` (
   `id` INT UNSIGNED NOT NULL AUTO_INCREMENT ,
   `name` VARCHAR(25) NOT NULL ,
   `parent_role_id` INT UNSIGNED NOT NULL DEFAULT NULL ,
@@ -31,16 +29,16 @@ CREATE  TABLE IF NOT EXISTS `NinjaAuthorization`.`role` (
   UNIQUE INDEX `name_UNIQUE` (`name` ASC) ,
   CONSTRAINT `fk_role_role_id`
     FOREIGN KEY (`parent_role_id` )
-    REFERENCES `NinjaAuthorization`.`role` (`id` )
+    REFERENCES `role` (`id` )
     ON DELETE NO ACTION
     ON UPDATE NO ACTION)
 ENGINE = InnoDB;
 
 
 -- -----------------------------------------------------
--- Table `NinjaAuthorization`.`role_assignment`
+-- Table `role_assignment`
 -- -----------------------------------------------------
-CREATE  TABLE IF NOT EXISTS `NinjaAuthorization`.`role_assignment` (
+CREATE  TABLE IF NOT EXISTS `role_assignment` (
   `id` INT UNSIGNED NOT NULL AUTO_INCREMENT ,
   `user_id` INT UNSIGNED NOT NULL ,
   `role_id` INT UNSIGNED NOT NULL ,
@@ -50,21 +48,21 @@ CREATE  TABLE IF NOT EXISTS `NinjaAuthorization`.`role_assignment` (
   PRIMARY KEY (`id`) ,
   CONSTRAINT `fk_role_assignment_user_id`
     FOREIGN KEY (`user_id` )
-    REFERENCES `NinjaAuthorization`.`user` (`id` )
+    REFERENCES `user` (`id` )
     ON DELETE NO ACTION
     ON UPDATE NO ACTION,
   CONSTRAINT `fk_role_assignment_role_id`
     FOREIGN KEY (`role_id` )
-    REFERENCES `NinjaAuthorization`.`role` (`id` )
+    REFERENCES `role` (`id` )
     ON DELETE NO ACTION
     ON UPDATE NO ACTION)
 ENGINE = InnoDB;
 
 
 -- -----------------------------------------------------
--- Table `NinjaAuthorization`.`resource`
+-- Table `resource`
 -- -----------------------------------------------------
-CREATE  TABLE IF NOT EXISTS `NinjaAuthorization`.`resource` (
+CREATE  TABLE IF NOT EXISTS `resource` (
   `id` INT UNSIGNED NOT NULL AUTO_INCREMENT ,
   `name` VARCHAR(45) NOT NULL ,
   `deleted` TINYINT(1) NOT NULL DEFAULT 0 ,
@@ -76,9 +74,9 @@ ENGINE = InnoDB;
 
 
 -- -----------------------------------------------------
--- Table `NinjaAuthorization`.`privilege`
+-- Table `privilege`
 -- -----------------------------------------------------
-CREATE  TABLE IF NOT EXISTS `NinjaAuthorization`.`privilege` (
+CREATE  TABLE IF NOT EXISTS `privilege` (
   `id` INT UNSIGNED NOT NULL AUTO_INCREMENT ,
   `name` VARCHAR(45) NOT NULL ,
   `deleted` TINYINT(1) NOT NULL DEFAULT 0 ,
@@ -90,11 +88,11 @@ ENGINE = InnoDB;
 
 
 -- -----------------------------------------------------
--- Table `NinjaAuthorization`.`permission`
+-- Table `permission`
 -- -----------------------------------------------------
-CREATE  TABLE IF NOT EXISTS `NinjaAuthorization`.`permission` (
+CREATE  TABLE IF NOT EXISTS `permission` (
   `id` INT UNSIGNED NOT NULL AUTO_INCREMENT ,
-  `role_id` INT UNSIGNED NULL DEFAULT NULL ,
+  `role_id` INT UNSIGNED NOT NULL DEFAULT NULL ,
   `user_id` INT UNSIGNED NULL DEFAULT NULL ,
   `resource_id` INT UNSIGNED NULL DEFAULT NULL ,
   `privilege_id` INT UNSIGNED NULL DEFAULT NULL ,
@@ -104,22 +102,22 @@ CREATE  TABLE IF NOT EXISTS `NinjaAuthorization`.`permission` (
   PRIMARY KEY (`id`) ,
   CONSTRAINT `fk_permission_role_id`
     FOREIGN KEY (`role_id` )
-    REFERENCES `NinjaAuthorization`.`role` (`id` )
+    REFERENCES `role` (`id` )
     ON DELETE NO ACTION
     ON UPDATE NO ACTION,
   CONSTRAINT `fk_permission_user_id`
     FOREIGN KEY (`user_id` )
-    REFERENCES `NinjaAuthorization`.`user` (`id` )
+    REFERENCES `user` (`id` )
     ON DELETE NO ACTION
     ON UPDATE NO ACTION,
   CONSTRAINT `fk_permission_resource_id`
     FOREIGN KEY (`resource_id` )
-    REFERENCES `NinjaAuthorization`.`resource` (`id` )
+    REFERENCES `resource` (`id` )
     ON DELETE NO ACTION
     ON UPDATE NO ACTION,
   CONSTRAINT `fk_permission_privilege_id`
     FOREIGN KEY (`privilege_id` )
-    REFERENCES `NinjaAuthorization`.`privilege` (`id` )
+    REFERENCES `privilege` (`id` )
     ON DELETE NO ACTION
     ON UPDATE NO ACTION)
 ENGINE = InnoDB;
