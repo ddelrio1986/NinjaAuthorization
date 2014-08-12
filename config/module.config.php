30600015961969e94aec7562c19d7e390c4fdf72
Daniel Del Rio <ddelrio1986@gmail.com>
2014-08-11 20:42:08 -0400

    Update the config file

--- config/module.config.php
+++ config/module.config.php
@@ -1,26 +1,16 @@
 <?php
-/**
- * Module Configuration
- *
- * This is the module configuration for the NinjaAuthorization module.
- *
- * @package NinjaAuthorization;
- * @filesource
- */
-
-namespace NinjaAuthorization;
 
 return array(
     'doctrine' => array(
         'driver' => array(
-            __NAMESPACE__ . '_driver' => array(
+            'NinjaAuthorization_driver' => array(
                 'class' => 'Doctrine\ORM\Mapping\Driver\AnnotationDriver',
                 'cache' => 'array',
-                'paths' => array(__DIR__ . '/../src/' . __NAMESPACE__ . '/Entity'),
+                'paths' => array(__DIR__ . '/../src/' . 'NinjaAuthorization/Entity'),
             ),
             'orm_default' => array(
                 'drivers' => array(
-                    __NAMESPACE__ . '\Entity' => __NAMESPACE__ . '_driver',
+                    'NinjaAuthorization\Entity' => 'NinjaAuthorization_driver',
                 ),
             ),
         ),
@@ -31,29 +21,24 @@ return array(
     ),
     'service_manager' => array(
         'factories' => array(
-            'PermissionService' => function ($serviceLocator) {
-                return $serviceLocator->get(__NAMESPACE__ . '\Service\PermissionEntityService');
-            },
-            'PrivilegeService' => function ($serviceLocator) {
-                return $serviceLocator->get(__NAEMSPACE__ . '\Service\PrivilegeEntityService');
-            },
-            'ResourceService' => function ($serviceLocator) {
-                return $serviceLocator->get(__NAMESPACE__ . '\Service\ResourceEntityService');
-            },
-            'RoleService' => function ($serviceLocator) {
-                return $serviceLocator->get(__NAMESPACE__ . '\Service\RoleEntityService');
-            },
-            'RoleAssignmentService' => function ($serviceLocator) {
-                return $serviceLocator->get(__NAMESPACE__ . '\Service\RoleAssignmentEntityService');
-            },
+
+            // Entity services.
+            'NinjaAuthorization\Service\Permission' => 'NinjaAuthorization\Service\PermissionFactory',
+            'NinjaAuthorization\Service\Privilege' => 'NinjaAuthorization\Service\PrivilegeFactory',
+            'NinjaAuthorization\Service\Resource' => 'NinjaAuthorization\Service\ResourceFactory',
+            'NinjaAuthorization\Service\Role' => 'NinjaAuthorization\Service\RoleFactory',
+            'NinjaAuthorization\Service\RoleAssignment' => 'NinjaAuthorization\Service\RoleAssignmentFactory',
         ),
         'invokables' => array(
-            __NAMESPACE__ . '\Entity\Permission' => __NAMESPACE__ . '\Entity\Permission',
-            __NAMESPACE__ . '\Entity\Privilege' => __NAMESPACE__ . '\Entity\Privilege',
-            __NAMESPACE__ . '\Entity\Resource' => __NAMESPACE__ . '\Entity\Resource',
-            __NAMESPACE__ . '\Entity\Role' => __NAMESPACE__ . '\Entity\Role',
-            __NAMESPACE__ . '\Entity\RoleAssignment' => __NAMESPACE__ . '\Entity\RoleAssignment',
-            __NAMESPACE__ . '\Service\Acl' => __NAMESPACE__ . '\Service\Acl',
+
+            // Entity factories.
+            'NinjaAuthorization\Entity\PermissionFactory' => 'NinjaAuthorization\Entity\PermissionFactory',
+            'NinjaAuthorization\Entity\PrivilegeFactory' => 'NinjaAuthorization\Entity\PrivilegeFactory',
+            'NinjaAuthorization\Entity\ResourceFactory' => 'NinjaAuthorization\Entity\ResourceFactory',
+            'NinjaAuthorization\Entity\RoleFactory' => 'NinjaAuthorization\Entity\RoleFactory',
+            'NinjaAuthorization\Entity\RoleAssignmentFactory' => 'NinjaAuthorization\Entity\RoleAssignmentFactory',
+
+            'NinjaAuthorization\Service\Acl' => 'NinjaAuthorization\Service\Acl',
             'ZFAcl' => 'Zend\Permissions\Acl\Acl',
         ),
     ),
